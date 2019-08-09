612ecb1080c77e410675f88c8a62b99bb5cf75ee
Jhon Achata <jhonal183@gmail.com>
2019-08-09 11:26:29 -0500

    adicionar seeder de frases

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UsersTableSeeder::class);
-        // $this->call(PhrasesTableSeeder::class);
+        $this->call(PhrasesTableSeeder::class);
         // $this->call(UsersTableSeeder::class);
     }
 }
