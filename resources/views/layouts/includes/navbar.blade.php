12de9655d49f539182d25aee5539547b040e3ec9
Jhon Achata <jhonal183@gmail.com>
2019-08-09 16:55:24 -0500

    Agregado boton de toogle para el sidebar

--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -1,6 +1,8 @@
 <div class="br-header">
     <div class="br-header-left">
-        
+        <div class="navicon-left hidden-md-down">
+            <a id="btnLeftMenu" href=""><i class="icon ion-navicon-round"></i></a>
+        </div>
     </div>
     <!-- br-header-left -->
     <div class="br-header-right">
